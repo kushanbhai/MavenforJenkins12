90465a64aff76d0d47a1abbc92bd270482ff00d0
kushanbhai <kushanchoudhury6@gmail.com>
2024-04-26 14:11:37 +0530

    first commit

---
+++ src/test/java/mavenforjenkins/UITest.java
@@ -0,0 +1,21 @@
+package mavenforjenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UITest {
+
+	
+	@Test
+	public void startBrowser()
+	{
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		driver.quit();
+	}
+}
